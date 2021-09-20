083bee599f7cd883364a4fe3bd2a780d558dd5ff
QANstar <1435385924@qq.com>
2021-09-20 20:45:22 +0800

    导航栏

--- courseproject/src/App.vue
+++ courseproject/src/App.vue
@@ -1,46 +1,57 @@
 <template>
-  <el-container style="">
-    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-       <el-menu :default-openeds="['1', '3']">
-      <el-sub-menu index="1">
+  <el-container >
+    <el-aside width="200px" class="leftNav">
+      <el-menu :default-openeds="['1', '3']">
+        <el-sub-menu index="1">
           <template #title><i class="el-icon-menu"></i>选项1</template>
-          <el-menu-item index="1-1-1"><router-link to="/">Home</router-link> </el-menu-item>
+          <router-link to="/">
+            <el-menu-item index="1-1-1">Home</el-menu-item>
+          </router-link>
         </el-sub-menu>
-      <el-sub-menu index="2">
-        <template #title><i class="el-icon-menu"></i>导航二</template>
-        <el-menu-item-group>
-          <template #title>分组一</template>
-          <el-menu-item index="2-1"><router-link to="/about">About</router-link></el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="2-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-sub-menu index="2-4">
-          <template #title>选项4</template>
-          <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+        <el-sub-menu index="2">
+          <template #title><i class="el-icon-menu"></i>导航二</template>
+          <el-menu-item-group>
+            <template #title>分组一</template>
+            <router-link to="/about">
+              <el-menu-item index="2-1">About</el-menu-item>
+            </router-link>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="2-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="2-4">
+            <template #title>选项4</template>
+            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+          </el-sub-menu>
         </el-sub-menu>
-      </el-sub-menu>
-      <el-sub-menu index="3">
-        <template #title><i class="el-icon-setting"></i>导航三</template>
-        <el-menu-item-group>
-          <template #title>分组一</template>
-          <el-menu-item index="3-1">选项1</el-menu-item>
-          <el-menu-item index="3-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="3-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-sub-menu index="3-4">
-          <template #title>选项4</template>
-          <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+        <el-sub-menu index="3">
+          <template #title><i class="el-icon-setting"></i>导航三</template>
+          <el-menu-item-group>
+            <template #title>分组一</template>
+            <el-menu-item index="3-1">选项1</el-menu-item>
+            <el-menu-item index="3-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="3-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="3-4">
+            <template #title>选项4</template>
+            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+          </el-sub-menu>
         </el-sub-menu>
-        </el-sub-menu>
-    </el-menu>
+      </el-menu>
     </el-aside>
+    <el-container  class="contentMain">
+    <el-header style="text-align: left; font-size: 12px">
+      <span>前端教程</span>
+    </el-header>
+
     <el-main>
       <router-view/>
     </el-main>
+  </el-container>
+
   </el-container>
 </template>
 
--- courseproject/src/main.js
+++ courseproject/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import "@/assets/static/css/style.css"
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import './registerServiceWorker'
 
 const app = createApp(App)
 app.use(ElementPlus)
