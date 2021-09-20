cc70d4792cb4bdaa9b862d77e64053d291050208
QANstar <1435385924@qq.com>
2021-09-20 17:18:29 +0800

    导航栏

--- courseproject/src/App.vue
+++ courseproject/src/App.vue
@@ -1,9 +1,47 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <el-container style="">
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+       <el-menu :default-openeds="['1', '3']">
+      <el-sub-menu index="1">
+          <template #title><i class="el-icon-menu"></i>选项1</template>
+          <el-menu-item index="1-1-1"><router-link to="/">Home</router-link> </el-menu-item>
+        </el-sub-menu>
+      <el-sub-menu index="2">
+        <template #title><i class="el-icon-menu"></i>导航二</template>
+        <el-menu-item-group>
+          <template #title>分组一</template>
+          <el-menu-item index="2-1"><router-link to="/about">About</router-link></el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="2-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-sub-menu index="2-4">
+          <template #title>选项4</template>
+          <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+      <el-sub-menu index="3">
+        <template #title><i class="el-icon-setting"></i>导航三</template>
+        <el-menu-item-group>
+          <template #title>分组一</template>
+          <el-menu-item index="3-1">选项1</el-menu-item>
+          <el-menu-item index="3-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="3-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-sub-menu index="3-4">
+          <template #title>选项4</template>
+          <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+        </el-sub-menu>
+        </el-sub-menu>
+    </el-menu>
+    </el-aside>
+    <el-main>
+      <router-view/>
+    </el-main>
+  </el-container>
 </template>
 
 <style>
