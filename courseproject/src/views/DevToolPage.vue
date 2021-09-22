4950495751137f24f72180a2cfe3f418e525cb15
QANstar <1435385924@qq.com>
2021-09-22 22:59:57 +0800

    前言模块--开发者工具模块编辑

--- courseproject/src/views/DevToolPage.vue
+++ courseproject/src/views/DevToolPage.vue
@@ -17,11 +17,41 @@
       <p>开发者工具最常用的四个功能模块：元素（ELements）、控制台（Console）、源代码（Sources）、网络（Network）。</p>
       <div>
         <el-tabs v-model="activeName">
-        <el-tab-pane label="元素（Elements）" name="first">User</el-tab-pane>
-        <el-tab-pane label="控制台（Console）" name="second">Config</el-tab-pane>
-        <el-tab-pane label="源代码（Sources）" name="third">Role</el-tab-pane>
-        <el-tab-pane label="网络（Network）" name="fourth">Task</el-tab-pane>
-      </el-tabs>
+          <el-tab-pane label="元素（Elements）" name="first">
+            <div class="tabBoxs">
+              <div>
+                <ul>
+                  <li>
+                    可通过右键想要查看的元素或者点击开发者工具左上角的箭头来查看具体位置的元素。
+                  </li>
+                  <li>
+                    定位到想要查看的元素后可以在右边观察到元素的style等属性，可通过修改style来查看效果，此时的修改不影响到代码。
+                  </li>
+                </ul>
+              </div>
+              <div class="eleImg">
+                <img src="../assets/static/img/ELements.png">
+              </div>
+
+
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="控制台（Console）" name="second">
+            <div class="tabBoxs">
+              test2
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="源代码（Sources）" name="third">
+            <div class="tabBoxs">
+              test3
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="网络（Network）" name="fourth">
+            <div class="tabBoxs">
+              test4
+            </div>
+          </el-tab-pane>
+        </el-tabs>
       </div>
     </div>
   </div>
@@ -29,7 +59,13 @@
 
 <script>
 export default {
-  name: "DevToolPage"
+  name: "DevToolPage",
+  data() {
+    return {
+      activeName: 'first',
+
+    }
+  },
 }
 </script>
 
